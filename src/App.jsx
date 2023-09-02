322c7762a0b7c7837396f43aa56d89f41e63f1a7
Bimal Basari <basaribimal@gmail.com>
2023-09-02 10:54:07 +0530

    intial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
-// import Resume from "./components/Resume/ResumeNew";
+import Resume from "./components/Resume/ResumeNew";
 import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import "./App.css";
@@ -32,7 +32,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
-          {/* <Route path="/resume" element={<Resume />} /> */}
+          <Route path="/resume" element={<Resume />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
